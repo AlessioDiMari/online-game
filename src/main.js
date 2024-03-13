8c0a9b3d1dd752de14a0ba16180d72a7cbd9b2da
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-03-13 07:13:30 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.scss'
+import App from './App.vue'
+import '/node_modules/@fortawesome/fontawesome-free/css/all.min.css'
+
+createApp(App).mount('#app')
